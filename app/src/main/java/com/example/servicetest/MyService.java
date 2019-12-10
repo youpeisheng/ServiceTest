c8bd7f3bdce532f2dce8a111cd10184d9b4a136d
youpeisheng <1426890389@qq.com>
2019-12-10 17:07:34 +0800

    在MyService onCreate 内新增前台服务

--- app/src/main/java/com/example/servicetest/MyService.java
+++ app/src/main/java/com/example/servicetest/MyService.java
@@ -1,14 +1,25 @@
 package com.example.servicetest;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.icu.text.LocaleDisplayNames;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
+import androidx.core.app.NotificationCompat;
+
 public class MyService extends Service {
     private DownloadBinder mBinder=new DownloadBinder();
+    private static final String ID="channel_1";
+    private static final String NAME="前台服务";
+
     class DownloadBinder extends Binder{
         public void startDownload(){
             Log.d("MyService","startDownload");
@@ -33,6 +44,20 @@ public class MyService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d("MyService","onCreate MyService");
+        Intent intent=new Intent(this,MainActivity.class);
+        NotificationManager manager=(NotificationManager)getSystemService (NOTIFICATION_SERVICE);
+        PendingIntent pi=PendingIntent.getActivity(this,0,intent,0);
+        NotificationChannel channel=new NotificationChannel(ID,NAME, NotificationManager.IMPORTANCE_HIGH);
+        manager.createNotificationChannel (channel);
+        Notification notification=new NotificationCompat.Builder(this,ID)
+                .setContentTitle("This is content title")
+                .setContentText("This is content text")
+                .setWhen(System.currentTimeMillis())
+                .setContentIntent(pi)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
+                .build();
+        startForeground(1,notification);
     }
 
     @Override
